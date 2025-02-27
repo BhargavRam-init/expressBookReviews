31e290aed77b75ab712dec9c7e798bfa3bf51d24
Barghav Abhilash B R <156782930+Meow-Codes@users.noreply.github.com>
2025-02-27 13:38:44 +0530

    Completed Task 7: Login as a registered user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,32 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  return users.some((user) => user.username == username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  return users.some(
+    (user) => user.username == username && user.password == password
+  );
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res
+      .status(400)
+      .json({ message: "Both Username and Password fields are mandatory" });
+  }
+  if (authenticatedUser(username, password)) {
+    const accessToken = jwt.sign({ username }, "fingerprint_customer", {
+      expiresIn: "1h",
+    });
+    req.session.authorization = { accessToken, username };
+    return res.status(200).json({ message: "Login Successful", token: accessToken});
+  } else {
+    return res.status(401).json({ message: "Invalid Username or Password" });
+  }
 });
 
 // Add a book review
