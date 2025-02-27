07feaf75edcf19828c86f6d60d2998385679d973
Barghav Abhilash B R <156782930+Meow-Codes@users.noreply.github.com>
2025-02-27 13:45:02 +0530

    Completed Task 8: Add or modify a book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -36,8 +36,16 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const { review } = req.body;
+  const username = req.session.authorization.username;
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  books[isbn].reviews[username] = review;
+  return res
+    .status(200)
+    .json({ message: "Book review added/updated successfully" });
 });
 
 module.exports.authenticated = regd_users;
