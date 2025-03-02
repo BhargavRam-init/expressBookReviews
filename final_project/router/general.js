5c958a6345a025fc0a0ed3c8ca644fc6bc2777de
Barghav Abhilash B R <156782930+Meow-Codes@users.noreply.github.com>
2025-03-02 13:20:53 +0530

    Completed Task 10: Get list of books using async/await with Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     res
@@ -19,34 +19,38 @@ public_users.post("/register", (req,res) => {
   return res.status(200).json({ message: "User successfully registered" });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json(books);   // Returns the list of books that are mentioned in booksdb.js
+// Get the book list available in the shop using async/await with Axios
+public_users.get('/', async function (req, res) {
+  try {
+    return res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to fetch books" });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const {isbn} = req.params;
-  if (!books[isbn]){
-    return res.status(404).json({message: "Book not Found"});
+public_users.get('/isbn/:isbn', function (req, res) {
+  const { isbn } = req.params;
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not Found" });
   }
   return res.status(200).json(books[isbn]);
 });
-  
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const {author} = req.params;
+public_users.get('/author/:author', function (req, res) {
+  const { author } = req.params;
   const BooksbyAuthor = Object.values(books).filter(
     (book) => book.author == author
   );
-  if (BooksbyAuthor.length == 0){
-    return res.status(404).json({message: "No books found by this author"});
+  if (BooksbyAuthor.length == 0) {
+    return res.status(404).json({ message: "No books found by this author" });
   }
   return res.status(200).json(BooksbyAuthor);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   const { title } = req.params;
   const BooksbyTitle = Object.values(books).filter(
     (book) => book.title == title
@@ -57,8 +61,8 @@ public_users.get('/title/:title',function (req, res) {
   return res.status(200).json(BooksbyTitle);
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
   const { isbn } = req.params;
   if (!books[isbn]) {
     return res.status(404).json({ message: "Book not found" });
