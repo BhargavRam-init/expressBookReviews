ebf515be72c2f65ab00d0ad01066f2d6a7f69248
Barghav Abhilash B R <156782930+Meow-Codes@users.noreply.github.com>
2025-03-02 13:33:34 +0530

    Completed Task 11: Get book details by ISBN using async/await with Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,6 +5,17 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const getBookByISBN = async (isbn) => {
+  try {
+    if (!books[isbn]) {
+      throw new Error("Book not found");
+    }
+    return books[isbn];
+  } catch (error) {
+    throw new Error("Failed to fetch book details");
+  }
+};
+
 public_users.post("/register", (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
@@ -29,12 +40,15 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const { isbn } = req.params;
-  if (!books[isbn]) {
-    return res.status(404).json({ message: "Book not Found" });
+
+  try {
+    const book = await getBookByISBN(isbn);
+    return res.status(200).json(book);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
-  return res.status(200).json(books[isbn]);
 });
 
 // Get book details based on author
